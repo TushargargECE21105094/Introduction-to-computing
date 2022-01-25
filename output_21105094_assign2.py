da19b7cb1794534211e746fcc39258055409e0bc
TushargargECE21105094 <97088111+TushargargECE21105094@users.noreply.github.com>
2022-01-25 18:41:46 +0530

    Add files via upload

---
+++ output_21105094_assign2.py
@@ -0,0 +1,51 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: C:/Users/Ashu Garg/Desktop/Assignment 2 python.py ==========
+               Program 1
+Input string : Python is a case sensitive language
+               Part a)
+Length of the given string is : 35
+               Part b)
+Reversed string : egaugnal evitisnes esac a si nohtyP
+               Part c)
+Slice : a case sensitive
+               Part d)
+String after a replacement : Python is object oriented language
+               Part e)
+Index of "a" is : 10
+               Part f)
+String after another replacement : Pythonisacasesensitivelanguage
+
+========== RESTART: C:/Users/Ashu Garg/Desktop/Assignment 2 python.py ==========
+               Program 2
+Hey,Tushar garg here!
+My SID is 21105094
+I am from ECE department and my CGPA is 8.500000
+
+========== RESTART: C:/Users/Ashu Garg/Desktop/Assignment 2 python.py ==========
+               Program 3
+a&b:   8
+a|b:   58
+a^b:   50
+a<<2:  224 
+b<<2:  40
+a>>2:  14 
+b>>4:  0
+
+========== RESTART: C:/Users/Ashu Garg/Desktop/Assignment 2 python.py ==========
+Enter first number,x: 34
+Enter second number,y: 66
+Enter third number,z: 19
+y is geatest
+
+========== RESTART: C:/Users/Ashu Garg/Desktop/Assignment 2 python.py ==========
+Enter a string:
+Hi, I am Tushar.My last name is garg.
+Yes
+
+========== RESTART: C:/Users/Ashu Garg/Desktop/Assignment 2 python.py ==========
+Enter first length: 21
+Enter second length: 45
+Enter third length: 39
+Yes
