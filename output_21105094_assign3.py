c9453c45ea11b8b574de6ea92bcc501b1aef0781
TushargargECE21105094 <97088111+TushargargECE21105094@users.noreply.github.com>
2022-02-14 20:33:49 +0530

    Add files via upload

---
+++ output_21105094_assign3.py
@@ -0,0 +1,162 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Dell-Users/AppData/Local/Programs/Python/Python310/Python Assignment 3.py
+                Program 1
+Enter a word or a sentence(give space between words and puctuation marks): 
+Hello I am Peter Parker and I am Spiderman .
+hello  occurs  1  times 
+i  occurs  3  times 
+am  occurs  2  times 
+peter  occurs  1  times 
+parker  occurs  1  times 
+and  occurs  1  times 
+spiderman  occurs  1  times 
+.  occurs  1  times 
+Note: If you want frequency of every charachter,remove the space between words and any unexpected space at start or end(if there)
+
+= RESTART: C:/Users/Dell-Users/AppData/Local/Programs/Python/Python310/Python Assignment 3.py
+                Program 1
+Enter a word or a sentence(give space between words and puctuation marks): 
+helloall,brucespeaking
+h  occurs  1  times 
+e  occurs  3  times 
+l  occurs  4  times 
+o  occurs  1  times 
+a  occurs  2  times 
+,  occurs  1  times 
+b  occurs  1  times 
+r  occurs  1  times 
+u  occurs  1  times 
+c  occurs  1  times 
+s  occurs  1  times 
+p  occurs  1  times 
+k  occurs  1  times 
+i  occurs  1  times 
+n  occurs  1  times 
+g  occurs  1  times 
+
+= RESTART: C:/Users/Dell-Users/AppData/Local/Programs/Python/Python310/Python Assignment 3.py
+                Program 2
+Enter day(from 1 to 31): 28
+Enter month(from 1 to 12): 2
+Enter year(from 1800 to 2025): 1900
+Next date(dd,mm,yyyy) is: 
+ 1 , 3 , 1900
+
+= RESTART: C:/Users/Dell-Users/AppData/Local/Programs/Python/Python310/Python Assignment 3.py
+                Program 2
+Enter day(from 1 to 31): 28
+Enter month(from 1 to 12): 2
+Enter year(from 1800 to 2025): 2000
+Next date(dd,mm,yyyy) is: 
+ 29 , 2 , 2000
+
+= RESTART: C:/Users/Dell-Users/AppData/Local/Programs/Python/Python310/Python Assignment 3.py
+                Program 3
+Enter a list of numbers separated by commas: 
+11,23,44,11,12,25,15,25
+Squares of the corresponding numbers: 
+ [(11, 121), (12, 144), (15, 225), (23, 529), (25, 625), (44, 1936)]
+
+= RESTART: C:/Users/Dell-Users/AppData/Local/Programs/Python/Python310/Python Assignment 3.py
+                Program 4
+Enter grade points: 6
+Your Grade is +C and Average Performance.
+
+= RESTART: C:/Users/Dell-Users/AppData/Local/Programs/Python/Python310/Python Assignment 3.py
+                Program 5
+Enter number of lines to be printed: 6
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+
+= RESTART: C:/Users/Dell-Users/AppData/Local/Programs/Python/Python310/Python Assignment 3.py
+                Program 6
+Do you want to make an entry? (type 'Y' for 'Yes' and 'N' for 'No') 
+Y
+Note:
+1) SIDs are positive numbers.
+2) All names must be written with capital first letter.
+3) Full name should be written, especially if more than one students have same first and middle name(if there).
+4) No two(or more) students should be assigned the same SID. 
+
+Enter SID: 100
+Enter Name: Rahul Garg
+Do you want to make another entry? (type 'Y' for 'Yes' and 'N' for 'No') 
+Y
+Enter SID: 102
+Enter Name: Dinesh Saigal
+Do you want to make another entry? (type 'Y' for 'Yes' and 'N' for 'No') 
+Y
+Enter SID: 105
+Enter Name: Sarita Jindal
+Do you want to make another entry? (type 'Y' for 'Yes' and 'N' for 'No') 
+Y
+Enter SID: 103
+Enter Name: Anmol Gupta
+Do you want to make another entry? (type 'Y' for 'Yes' and 'N' for 'No') 
+Y
+Enter SID: 104
+Enter Name: Rahul Jain
+Do you want to make another entry? (type 'Y' for 'Yes' and 'N' for 'No') 
+N
+OK
+     Part a) 
+Details of student(s) : 
+ {100: 'Rahul Garg', 102: 'Dinesh Saigal', 105: 'Sarita Jindal', 103: 'Anmol Gupta', 104: 'Rahul Jain'}
+     Part b) 
+Details of student(s) after sorting by names: 
+ {103: 'Anmol Gupta', 102: 'Dinesh Saigal', 100: 'Rahul Garg', 104: 'Rahul Jain', 105: 'Sarita Jindal'}
+     Part c) 
+Details of student(s) after sorting by sid: 
+ {100: 'Rahul Garg', 102: 'Dinesh Saigal', 103: 'Anmol Gupta', 104: 'Rahul Jain', 105: 'Sarita Jindal'}
+     Part d) 
+Search details of a student: 
+
+Write SID of the student: 
+103
+Name of the student is : 
+ Anmol Gupta
+
+
+= RESTART: C:/Users/Dell-Users/AppData/Local/Programs/Python/Python310/Python Assignment 3.py
+                Program 7
+Enter the number of terms to be displayed in sequence: 10
+Fibonacci sequence (till 10 terms) :
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+Average of the above sequence's series is : 8.8
+
+= RESTART: C:/Users/Dell-Users/AppData/Local/Programs/Python/Python310/Python Assignment 3.py
+                Program 8
+Given sets :
+ Set1 : {1, 2, 3, 4, 5}
+ Set2 : {8, 2, 4, 6}
+ Set3 : {1, 17, 5, 9, 13}
+     Part a) 
+Set of elements that are in Set1 and Set2 but not in both :
+ {1, 3, 5, 6, 8}
+     Part b) 
+Set of elements that are in only one of Set1,Set2 and Set3 :
+ {17, 3, 6, 8, 9, 13}
+     Part c) 
+Set of elements that are in exactly two of Set1,Set2 and Set3 :
+ {1, 2, 4, 5}
+     Part d) 
+Set of integers from 1 to 10 that are not in Set1 :
+ {6, 7, 8, 9, 10}
+     Part e) 
+Set of integers from 1 to 10 that are not in Set1,Set2 and Set3 :
+ {10, 7}
