240085039387460d9892d180cc014db9bcd23889
TushargargECE21105094 <97088111+TushargargECE21105094@users.noreply.github.com>
2022-03-02 00:05:12 +0530

    Add files via upload

---
+++ output_21105094_assign4.py
@@ -0,0 +1,109 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: C:/Users/Dell-Users/Desktop/assignment 4 python.py =========
+                    Ques 1
+Three disks are in 'A' rod initially and will be in 'B' rod finally .
+
+Move disk 1 from A to B
+Move disk 2 from A to C
+Move disk 1 from B to C
+Move disk 3 from A to B
+Move disk 1 from C to A
+Move disk 2 from C to B
+Move disk 1 from A to B
+
+
+
+
+                    Ques 2
+Using recursions 1
+Enter number of rows to be printed: 3
+    1   
+   1   1   
+  1   2   1   
+
+
+
+Using recursions 2
+Enter number of rows to be printed: 4
+   1    
+  1    1    
+ 1    2    1    
+1    3    3    1    
+
+
+
+Using iterations
+Enter number of rows to be printed: 5
+       1
+      1  1
+     1  2  1
+    1  3  3  1
+   1  4  6  4  1
+
+
+
+
+                    Ques 3
+Enter first number: 45
+Enter second number(should be non-zero): -15
+ Quoteint is:  -3
+ Remiander is:  0
+Function is callable.
+Remainder is zero but Quotient is non zero.
+New result : (-3, 0, 4, 5, 6)
+Values greater than 4 are:
+ (5, 6)
+After converting to Set datatype:
+ {5, 6}
+Immutable Set:
+ frozenset({5, 6})
+Largest value in the set:  6
+Hash value of the largest value in the set: 6
+
+
+
+
+                    Ques 4
+Object created for Rahul Garg
+Object created for Manushi Jain
+Object created for Jagdish Mishra
+Object created for Anmol Gupta
+Name of student Anmol Gupta
+Roll no. of student 17
+Name of student Jagdish Mishra
+Roll no. of student 25
+Name of student Manushi Jain
+Roll no. of student 49
+Name of student Rahul Garg
+Roll no. of student 53
+Object deleted for Anmol Gupta
+Object deleted for Jagdish Mishra
+Object deleted for Manushi Jain
+Object deleted for Rahul Garg
+
+
+
+
+                    Ques 5
+Name of Employee Mehak
+Salary of Employee 40000
+Name of Employee Ashok
+Salary of Employee 50000
+Name of Employee Viren
+Salary of Employee 60000
+The updated salary of Mehak is 70000.
+Viren's record is deleted
+
+
+
+
+                    Ques 6
+Enter a word: antman
+Enter a new meaningful word to test your friendship:tannam
+OOPS! You and your friend failed the friendship test.
+
+
+
+
